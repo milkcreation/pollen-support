e7ee440b4bf8ef453007dc2816c2b4ee3ebaf72b
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-02-11 10:56:13 +0000

    support >> 1.0.x-dev

---
+++ support/branches/1.0/src/Env.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Support;
+
+use Illuminate\Support\Env as BaseEnv;
+
+class Env extends BaseEnv
+{
+    /**
+     * Vérifie si l'environnement d'éxecution est en développement.
+     *
+     * @return bool
+     */
+    public static function isDev(): bool
+    {
+        return static::get('APP_ENV') === 'dev' || static::get('APP_ENV') === 'developpement';
+    }
+
+    /**
+     * Vérifie si l'environnement d'éxecution est en production.
+     *
+     * @return bool
+     */
+    public static function isProd(): bool
+    {
+        return static::get('APP_ENV') === 'prod' || static::get('APP_ENV') === 'production';
+    }
+
+    /**
+     * Vérifie si l'environnement d'éxecution est en recette.
+     *
+     * @return bool
+     */
+    public static function isStaging(): bool
+    {
+        return static::get('APP_ENV') === 'staging';
+    }
+}
