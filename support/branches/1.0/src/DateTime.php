11f0e0b8f74fb4e673d9bc45b5958bee01ad5bdb
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-04-24 12:16:04 +0000

    support >> 1.0.x-dev

--- support/branches/1.0/src/DateTime.php
+++ support/branches/1.0/src/DateTime.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace Pollen\Support;
 
 use Carbon\Carbon;
-use DateTime as BaseDateTime;
-use DateTimeZone;
 use Exception;
-use Pollen\Http\Request;
 
 class DateTime extends Carbon
 {
@@ -19,51 +16,11 @@ class DateTime extends Carbon
     protected static $defaultFormat = 'Y-m-d H:i:s';
 
     /**
-     * Instance du fuseau horaire utilisé par défaut.
-     * @var DateTimeZone
-     */
-    protected static $globalTimeZone;
-
-    /**
-     * @param string|null $time
-     * @param null|DateTimeZone $tz
-     *
-     * @throws Exception
-     */
-    public function __construct($time = null, $tz = null)
-    {
-        if (is_null($tz)) {
-            $tz = static::getGlobalTimeZone();
-        }
-        parent::__construct($time, $tz);
-    }
-
-    /**
      * @inheritDoc
      */
     public function __toString(): string
     {
         return $this->format(static::$defaultFormat);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @return BaseDateTime|static|null
-     */
-    public static function createFromFormat($format, $time, $timezone = null)
-    {
-        return parent::createFromFormat($format, $time, is_null($timezone) ? static::getGlobalTimeZone() : $timezone);
-    }
-
-    /**
-     * Récupération du fuseau horaire par défaut.
-     *
-     * @return DateTimeZone
-     */
-    public static function getGlobalTimeZone(): DateTimeZone
-    {
-        return static::$globalTimeZone ?: static::setGlobalTimeZone();
     }
 
     /**
@@ -76,23 +33,6 @@ class DateTime extends Carbon
     public static function setDefaultFormat(string $format): string
     {
         return static::$defaultFormat = $format;
-    }
-
-    /**
-     * Définition du fuseau horaire par défaut.
-     *
-     * @param DateTimeZone|null $tz
-     *
-     * @return DateTimeZone
-     */
-    public static function setGlobalTimeZone(?DateTimeZone $tz = null): DateTimeZone
-    {
-        return static::$globalTimeZone = $tz ?: new DateTimeZone(
-            Env::get('APP_TIMEZONE') ?: Request::getFromGlobals()->server->get(
-                'TZ',
-                ini_get('date.timezone') ?: 'UTC'
-            )
-        );
     }
 
     /**
