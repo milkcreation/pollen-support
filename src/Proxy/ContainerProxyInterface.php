f56550e2afd2630de3d81b68b9ca20c6608a71cd
Jordy Manner <jordy.manner@milkcreation.fr>
2021-05-03 09:32:45 +0000

    support >> 1.0.x-dev

--- src/Proxy/ContainerProxyInterface.php
+++ src/Proxy/ContainerProxyInterface.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace Pollen\Support\Proxy;
 
+use League\Container\Definition\DefinitionInterface;
 use Psr\Container\ContainerInterface as Container;
 
 interface ContainerProxyInterface
 {
+    /**
+     * Ajout d'un service fourni par le conteneur d'injection de dépendances.
+     *
+     * @param string $alias Alias de qualification du service.
+     * @param mixed|null $concrete
+     * @param bool $share
+     *
+     * @return DefinitionInterface
+     */
+    public function containerAdd(string $alias, $concrete = null, bool $share = false): ?DefinitionInterface;
+
     /**
      * Vérification de disponibilité d'un service fourni par le conteneur d'injection de dépendances.
      *
